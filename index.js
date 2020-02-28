c292f252fa7058579f3b6945948118629f640f74
Shaqqour <mshaqqourm@gmail.com>
2020-02-28 11:56:48 -0800

    Done.

--- index.js
+++ index.js
@@ -2,5 +2,5 @@
 document.querySelector('main#main').remove();
 let newHeader = document.createElement("h1");
 newHeader.id = "victory";
-newHeader.innerHTML = "Shaqqour";
+newHeader.innerHTML = "Shaqqour is the champion";
 document.body.appendChild(newHeader);
